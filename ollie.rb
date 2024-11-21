a9df1b8b1801f72a881ea4d17440b49754d67657
Lucas Paim <lucasmpaim1@gmail.com>
2024-11-21 10:12:36 -0300

    chore: update goreleaser binary name

--- ollie.rb
+++ ollie.rb
@@ -1,3 +1,4 @@
+require "formula"
 require_relative "./lib/download_strategy"
 
 class Ollie < Formula
@@ -8,16 +9,16 @@ class Ollie < Formula
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/TeamGoodOnes/ollie-cli/releases/download/v0.0.1/ollie-cli_Darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-        sha256 "f30ad2d07369569e52d8407165c2f24f0e8d7faa10883a7e33e1645d3b269222"
+        url "https://github.com/TeamGoodOnes/ollie-cli/releases/download/v0.0.2/ollie-cli_Darwin_arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        sha256 "1e72809d15988bb21e38d8f04b5263225b0c5e678c1a20db82de921f468a0625"
       
         def install
           bin.install "ollie"
         end
       end
       if Hardware::CPU.intel?
-        url "https://github.com/TeamGoodOnes/ollie-cli/releases/download/v0.0.1/ollie-cli_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-        sha256 "8a08ea19ff270d6f61f0be4f40cc23c1ead577fd402ae652f130399df63e7803"
+        url "https://github.com/TeamGoodOnes/ollie-cli/releases/download/v0.0.2/ollie-cli_Darwin_x86_64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+        sha256 "7e10ae9a6984f1be7b0061144792e29dc820c86a11eac8097dcd8fa0c0ab0811"
   
         def install
           bin.install "ollie"
